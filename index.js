40ae9786d462ee305a27cbcb1245700c612a26ef
Kyle Ortiz <kyleaortiz@gmail.com>
2021-06-30 15:05:40 -0500

    commit tree 28db5bbb29bc75d188787fe00c8902aaa8f5b7af

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (arrayOfDrivers) {
+  return arrayOfDrivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (arrayOfDrivers) {
+  return arrayOfDrivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+  return function (fare) {
+    return fare * integer;
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(
+  arrayOfDrivers,
+  returnFirstTwoDrivers = false,
+  returnLastTwoDrivers = false
+) {
+  if (returnFirstTwoDrivers != false) {
+    return returnFirstTwoDrivers(arrayOfDrivers);
+  }
+  if (returnLastTwoDrivers != false) {
+    return returnLastTwoDrivers(arrayOfDrivers);
+  }
+}
